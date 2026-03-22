e34b059331b4f7d610c4adfc442f839cc2a4e708
Venomseye <103646076+Venomseye@users.noreply.github.com>
2026-03-22 17:24:10 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Installing dependencies..."
+sudo pacman -S --needed ncurses fftw libpulse
+
+echo "Building..."
+g++ visualizer.cpp -o visualizer \
+    -lncursesw -lpulse-simple -lpulse -lfftw3 -lm -O2 -std=c++17
+
+echo "Installing to /usr/local/bin..."
+sudo install -m755 visualizer /usr/local/bin/visualizer
+
+echo "Done! Run with: visualizer"
